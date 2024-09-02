857557a8dd051e542b5ff0f293db2ba1c949319c
Ilya Boiko <boiko.ilya.evg@gmail.com>
2024-09-02 20:30:39 +0500

    _5_5_3_2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,35 +17,38 @@ namespace SF_CS_005
             Console.WriteLine("Укажите глубину эха");
             var Deep = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(SomeString);
             Echo(SomeString, Deep);
 
             Console.ReadKey();
         }
 
-        static void Echo(string IncomeString, int Deep, int Step = 1)
+        static void Echo(string IncomeString, int Deep, string MissingLetters = "")
         {
             string NextString = IncomeString;
 
             if (NextString.Length > 1)
             {
-                NextString = NextString.Remove(0, Math.Min(NextString.Length, 1));
+                if (NextString.Length == 1)
+                {
+                    MissingLetters = MissingLetters + ".";
+                } else
+                {
+                    MissingLetters = MissingLetters + "..";
+                }
+
+                NextString = NextString.Remove(0, Math.Min(NextString.Length, 2));
             }
             else
             {
                 return;
             }
 
-            string MissingLetters = "";
-            for (int i = 0; i < Step; i++)
-            {
-                MissingLetters = MissingLetters + ".";
-            }
-
             Console.WriteLine("{0}{1}", MissingLetters, NextString);
 
             if (Deep > 1)
             {
-                Echo(NextString, Deep - 1, Step + 1);
+                Echo(NextString, Deep - 1, MissingLetters);
             }
         }
     }
